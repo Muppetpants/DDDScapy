7fd1d34a3bfc74fe5ed36f334225efa9201e3018
Muppetpants <jason.gray@student.sans.edu>
2024-04-25 17:06:32 -0400

    Update README.md

--- DDDScapy.py
+++ DDDScapy.py
@@ -1,24 +1,31 @@
 from scapy.all import *
- 
+
+# Vars
 recipients_mac_adress= 'ff:ff:ff:ff:ff:ff'
 your_mac_adress= 'de:ad:be:ef:ca:fe'  #adjust as required
 ssid = 'MuppetPants' #adjust as required
 channel = chr(6)  #adjust as required
 interface = 'wlan0mon'  #adjust as required
- 
-frame= RadioTap()\
+# Using readlines()
+file1 = open('file.txt', 'r')
+Lines = file1.readlines()
+count = 0
+
+# Strips the newline character
+for line in Lines:
+    count += 1
+    print("Line{}: {}".format(count, line.strip()))
+    new_line = bytes(line.strip(), 'utf-8')
+    frame= RadioTap()\
       /Dot11(type=0, subtype=4, addr1=recipients_mac_adress, addr2=your_mac_adress, addr3=recipients_mac_adress)\
       /Dot11ProbeReq()\
       /Dot11Elt(ID='SSID', info=ssid)\
       /Dot11Elt(ID='Rates', info='\x82\x84\x8b\x96\x0c\x12\x18')\
       /Dot11Elt(ID='ESRates', info='\x30\x48\x60\x6c')\
       /Dot11Elt(ID='DSset', info=channel)\
-      /Dot11Elt(ID='Vendor',info=b'\x00\x50\xf2\x99'
-      b'' #Add payload from encryptThings.sh between the quotes
-      )
-
-frame.show()
-print("\nHexdump of frame:")
-hexdump(frame)
-#wrpcap("DDDScapy.pcap",frame) #Uncomment to create/test pcap
-sendp(frame, iface=interface, inter=5.00, loop=1) #adjust interval as required
+      /Dot11Elt(ID='Vendor',info=b'\x00\x50\xf2\x99'+ new_line)
+    wrpcap("DDDScapy" + str(count) + ".pcap",frame) #Uncomment to create/test pcap
+    frame.show()
+    print("\nHexdump of frame:")
+    hexdump(frame)
+    #sendp(frame, iface=interface, count=10, inter=1./10)) #adjust interval as required
--- README.md
+++ README.md
@@ -2,12 +2,5 @@
 
 Hacky AF, working on a complete python build. 
 
-This attempts to include encrypted payloads within a probe request information element. Looks weird, but who's looking at probes? (We are!) 
+Include multi-line plaintext payload within a probe request information element. 
 
-1. Ensure adapter is in monitor mode and camped on desired channel.
-2. Use Encrypt script to build payload and transmit 
-3. Extract payload (IEEE 802.11 Wireless Management>Tag: Vendor Specific, Right click, copy, as Hex Stream) 
-4. Paste entire hex stream into decrypt script
-5. Profit
-
-Will eventually consolidate everything into a single python tool, look at stuffing Beacons, and maybe parse through encrypted text files, line by line. Maybe.
